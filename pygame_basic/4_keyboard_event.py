14abdf6dfb75a58b0620e78e127af5b419c8cd59
KIMTAEKKWON <cheng80@nate.com>
2025-10-02 16:26:58 +0900

    캐릭터 이동 기능 구현 및 방향키 입력 처리 추가

--- pygame_basic/4_keyboard_event.py
+++ pygame_basic/4_keyboard_event.py
@@ -21,6 +21,11 @@ character_height = character_size[1] #캐릭터의 세로 크기
 character_x_pos = (screen_width * 0.5) - (character_width * 0.5) #화면 가로의 절반 크기에 해당하는 곳에 위치
 character_y_pos = screen_height - character_height #화면 세로 크기 가장 아래에 해당하는 곳에 위치
 
+# 이동할 좌표
+to_x = 0
+to_y = 0
+
+
 # FPS
 clock = pygame.time.Clock()
 
@@ -33,7 +38,40 @@ while running:
             running = False #게임이 진행중이 아님
         else : 
             pygame.display.update()
+
+        if event.type == pygame.KEYDOWN: #키가 눌러졌는지 확인
+            if event.key == pygame.K_LEFT: #캐릭터를 왼쪽으로
+                # character_x_pos -= 5
+                to_x -= 5
+            elif event.key == pygame.K_RIGHT: #캐릭터를 오른쪽으로
+                # character_x_pos += 5
+                to_x += 5
+            elif event.key == pygame.K_UP: #캐릭터를 위로
+                # character_y_pos -= 5
+                to_y -= 5
+            elif event.key == pygame.K_DOWN: #캐릭터를 아래로
+                # character_y_pos += 5
+                to_y += 5
+        
+        if event.type == pygame.KEYUP: #방향키를 떼면 멈춤
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                to_x = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                to_y = 0
     
+    character_x_pos += to_x
+    character_y_pos += to_y
+
+    if character_x_pos < 0:
+        character_x_pos = 0
+    elif character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width - character_width
+    if character_y_pos < 0:
+        character_y_pos = 0
+    elif character_y_pos > screen_height - character_height:
+        character_y_pos = screen_height - character_height
+
+
     screen.blit(background,(0,0)) #배경 그리기
     screen.blit(character,(character_x_pos,character_y_pos)) #캐릭터 그리기
 
